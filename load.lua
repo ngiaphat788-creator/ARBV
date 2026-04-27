19bc7db81a1e82e08df80a31c3c472fe1314a5c2
ngiaphat788-creator <ngiaphat788@gmail.com>
2026-04-27 07:23:18 +0700

    Update load.lua for C00LGUI edition enhancements
    Refactor UI library and add new features for speed and jump adjustments.

--- load.lua
+++ load.lua
@@ -1,20 +1,38 @@
--- ARBV Multi-Platform Support (Global & VNG)
-local Kavo = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Kavo.CreateLib("ARBV PRIVATE - ALL VERSION (VNG/GLOBAL)", "DarkTheme")
+-- [[ ARBV SYSTEM - C00LGUI EDITION ]] --
+-- [[ CREATED BY NGIAPHAT788 DEVELOPER ]] --
 
-local Main = Window:NewTab("Hack VNG/Global")
-local Section = Main:NewSection("Status: Hoạt động tốt")
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
--- Chức năng chỉnh tốc độ (Dùng được cho cả 2 bản)
-Section:NewSlider("Tốc Độ (Speed)", "Chạy nhanh hơn", 500, 16, function(s)
+-- CHUYỂN SANG BLOODTHEME CHO NÓ CHẤT
+local Window = Library.CreateLib("C00LGUI X ARBV - TUBERS93 EDITION", "BloodTheme")
+
+-- TẠO CÁC TAB NHÌN NGUY HIỂM
+local Tab1 = Window:NewTab("HỦY DIỆT")
+local Section1 = Tab1:NewSection("Status: SẴN SÀNG BÁO")
+
+Section1:NewSlider("TỐC ĐỘ (SPEED)", "Chỉnh tốc độ chạy", 1000, 16, function(s)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
 
--- Nút Fly (Bay) - Tương thích mọi phiên bản
-Section:NewButton("Bay Lượn (Fly)", "Bật Infinite Yield", function()
+Section1:NewSlider("NHẢY CAO (JUMP)", "Chỉnh lực nhảy", 1000, 50, function(s)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+    game.Players.LocalPlayer.Character.Humanoid.UseJumpPower = true
+end)
+
+local Tab2 = Window:NewTab("BAY LƯỢN")
+local Section2 = Tab2:NewSection("Nút Bay của Tubers93")
+
+Section2:NewButton("BẬT BAY (FLY)", "Bật Infinite Yield", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Edgeiy/infiniteyield/master/source"))()
 end)
 
-Section:NewKeybind("Đóng/Mở Menu", "Nhấn để ẩn menu", Enum.KeyCode.RightControl, function()
-	Kavo:ToggleUI()
+local Tab3 = Window:NewTab("THÔNG TIN")
+local Section3 = Tab3:NewSection("Hacker Info")
+Section3:NewLabel("Hacker: Ngiaphat788")
+Section3:NewLabel("Hỗ trợ: VNG / GLOBAL")
+Section3:NewLabel("Phiên bản: 6.6.6") -- Số đẹp cho giống c00lgui
+
+-- PHÍM TẮT ĐỂ GIẤU BẢNG (Right Control)
+Section3:NewKeybind("Phím Ẩn Menu", "Nhấn R-Ctrl", Enum.KeyCode.RightControl, function()
+	Library:ToggleUI()
 end)
