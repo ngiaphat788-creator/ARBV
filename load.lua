af280656e2bf7d95784a65890f9a1a565a9b0621
ngiaphat788-creator <ngiaphat788@gmail.com>
2026-04-27 07:14:58 +0700

    Update UI library and features for VNG/Global

--- load.lua
+++ load.lua
@@ -1,13 +1,20 @@
+-- ARBV Multi-Platform Support (Global & VNG)
 local Kavo = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Kavo.CreateLib("ARBV PRIVATE SYSTEM v5.1", "DarkTheme")
+local Window = Kavo.CreateLib("ARBV PRIVATE - ALL VERSION (VNG/GLOBAL)", "DarkTheme")
 
-local Main = Window:NewTab("Tính Năng")
-local Section = Main:NewSection("Người tạo: Ngiaphat788")
+local Main = Window:NewTab("Hack VNG/Global")
+local Section = Main:NewSection("Status: Hoạt động tốt")
 
-Section:NewSlider("Tốc Độ", "Chỉnh tốc độ chạy", 500, 16, function(s)
+-- Chức năng chỉnh tốc độ (Dùng được cho cả 2 bản)
+Section:NewSlider("Tốc Độ (Speed)", "Chạy nhanh hơn", 500, 16, function(s)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
 
-Section:NewButton("Bay (Fly)", "Bật Infinite Yield", function()
+-- Nút Fly (Bay) - Tương thích mọi phiên bản
+Section:NewButton("Bay Lượn (Fly)", "Bật Infinite Yield", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Edgeiy/infiniteyield/master/source"))()
+end)
+
+Section:NewKeybind("Đóng/Mở Menu", "Nhấn để ẩn menu", Enum.KeyCode.RightControl, function()
+	Kavo:ToggleUI()
 end)
